b68d489fea6d01dedbca89c225bc714686e103c0
Sebastian Karlsson <ianertson@gmail.com>
2021-06-23 17:50:47 +0200

    write main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,46 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
 
-// once you see the print message, raise your hand
 int main(int argc, char* argv[])
 {
-  printf("hello!\n");
+  SDL_Window *win = NULL;
+  SDL_Renderer *renderer = NULL;
+  int w, h;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("Error!\n");
+    return 1;
+  }
+
+  // create window
+  win = SDL_CreateWindow("Pong", 0, 0, 640, 480, 0);
+
+  // create renderer
+  renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+  while (true) {
+
+    // looking for user input
+    SDL_Event e;
+    if (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT)
+        break;
+    }
+
+    // update the graphics
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+  }
+
+  // destroying the window and the reference to the renderer
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(win);
+
   return 0;
 }
+
 
 // cmake . -G "Unix Makefiles"
 // make
